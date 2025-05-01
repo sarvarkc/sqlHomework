a230a4393e818329246bcf8a792b53f56bc27139
sarvarkc <itdevor@gmail.com>
2025-05-01 12:45:26 +0500

    lesson-10

---
+++ lesson-10/homework/SQLQuery1.sql
@@ -0,0 +1,61 @@
+CREATE TABLE Shipments (
+    N INT PRIMARY KEY,
+    Num INT
+);
+
+INSERT INTO Shipments (N, Num) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 2),
+(10, 2),
+(11, 2),
+(12, 2),
+(13, 2),
+(14, 4),
+(15, 4),
+(16, 4),
+(17, 4),
+(18, 4),
+(19, 4),
+(20, 4),
+(21, 4),
+(22, 4),
+(23, 4),
+(24, 4),
+(25, 4),
+(26, 5),
+(27, 5),
+(28, 5),
+(29, 5),
+(30, 5),
+(31, 5),
+(32, 6),
+(33, 7);
+
+;with cte as (
+	select max(N)+1 as MaxN,0 as NN
+	from Shipments
+	union all
+	select MaxN+1,NN
+	from cte
+	where MaxN < 40
+)
+
+insert into Shipments
+select MaxN, NN from cte
+
+;WITH Ordered AS (
+    SELECT Num,
+           ROW_NUMBER() OVER (ORDER BY Num) AS rn
+    FROM Shipments
+),
+MedianRows AS (
+    SELECT Num FROM Ordered WHERE rn IN (20, 21)
+)
+SELECT AVG(Num * 1.0) AS Median FROM MedianRows;
